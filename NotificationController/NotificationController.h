b94e93a37e22d23c90b018e6b570c3ffdc964627
kukushi <17washabi@gmail.com>
2015-11-04 23:52:47 +0800

    Initial Commit

---
+++ NotificationController/NotificationController.h
@@ -0,0 +1,19 @@
+//
+//  NotificationController.h
+//  NotificationController
+//
+//  Created by Xing He on 11/4/15.
+//  Copyright © 2015 Xing He. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NotificationController.
+FOUNDATION_EXPORT double NotificationControllerVersionNumber;
+
+//! Project version string for NotificationController.
+FOUNDATION_EXPORT const unsigned char NotificationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NotificationController/PublicHeader.h>
+
+
---
+++ NotificationControllerTests/NotificationControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  NotificationControllerTests.swift
+//  NotificationControllerTests
+//
+//  Created by Xing He on 11/4/15.
+//  Copyright © 2015 Xing He. All rights reserved.
+//
+
+import XCTest
+@testable import NotificationController
+
+class NotificationControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
